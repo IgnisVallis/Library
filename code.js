b383dd3585deb98dd18da3371b4df250cfe50d29
IgnisVallis <nacho22389@gmail.com>
2023-09-03 20:06:09 -0300

    uwu

--- code.js
+++ code.js
@@ -1,3 +1,7 @@
+const addButton = document.querySelector(".add-button");
+const windowAdd = document.querySelector(".window")
+const quitButton = document.querySelector(".quit-button")
+
 const libray = [];
 
 function Book(name,author,pages,read){
@@ -11,3 +15,13 @@ this.read = read;
 function addBookToLibrary(){
 
 }
+
+
+
+addButton.addEventListener("click", e =>{
+   windowAdd.style.display = "flex";
+})
+
+quitButton.addEventListener("click", e=>{
+    windowAdd.style.display = "none";
+})
